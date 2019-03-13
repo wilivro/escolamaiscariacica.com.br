bf287ce8f9620f53694bd0427a88afffe95cc682
Danilo Thiago <danilo.t.a.oliveira@hotmail.com>
2019-03-13 10:28:49 -0300

    menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 $("#menu img").click(function() {
-  $("#sub-menu").animate({left: 0});
+  $("#sub-menu").animate({width: '70%'},200);
 });
 
 $("#sub-menu img").click(function() {
-  $("#sub-menu").animate({left: -400});
+  $("#sub-menu").animate({width: '0'},200);
 });
